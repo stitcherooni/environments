d3f390077a1fca3047011eceda305f066f0f6c29
Stanislav Bahmet <extstbah@pandora.net>
2023-09-29 10:03:44 +0300

    add qa database and dev + qa namespace

--- internal/infra/main.tf
+++ internal/infra/main.tf
@@ -4,11 +4,34 @@ resource "random_password" "mysql_admin_password" {
   special = false
 }
 
-#Create MySQL database
+#Create MySQL dev database
 resource "azurerm_mysql_flexible_database" "ptaevent_dev" {
   name                = "ptaevent_dev"
   resource_group_name = module.infra.resource_group_name
   server_name         = local.mysql_conf.internal_mysql.name
   charset             = "latin1"
   collation           = "latin1_swedish_ci"
+}
+
+#Create MySQL qa database
+resource "azurerm_mysql_flexible_database" "ptaevent_qa" {
+  name                = "ptaevent_qa"
+  resource_group_name = module.infra.resource_group_name
+  server_name         = local.mysql_conf.internal_mysql.name
+  charset             = "latin1"
+  collation           = "latin1_swedish_ci"
+}
+
+#Create dev Namespace
+resource "kubernetes_namespace_v1" "dev" {
+  metadata {
+    name = "dev"
+  }
+}
+
+#Create qa Namespace
+resource "kubernetes_namespace_v1" "qa" {
+  metadata {
+    name = "qa"
+  }
 }
